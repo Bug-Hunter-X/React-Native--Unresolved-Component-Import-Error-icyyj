1e41a9155f0b930771cf389b2b72d66054508f5d
BugHunter-X <askzenai@gmail.com>
2024-12-18 15:42:09 +0100

    Add FixedComponent.js file

---
+++ FixedComponent.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import MyComponent from './MyComponent'; // Import missing component
+
+const FixedComponent = () => {
+  return (
+    <MyComponent />
+  );
+};
+
+export default FixedComponent;
